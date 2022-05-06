01a56c821a452b89c1303ef09fa87a4e68166756
Salo25 <92531211+Salo25@users.noreply.github.com>
2022-05-06 15:10:24 +0200

    v2

--- Proyecto trimestre 3/mediamarkNuevo~3.sql
+++ Proyecto trimestre 3/mediamarkNuevo~3.sql
@@ -24,6 +24,8 @@ SELECT c.nombre, p.nombre
 SELECT c.nombre, p.clientes_id, p.importe, p.fecha, pr.nombre
     FROM clientes c, pedidos p, linea_pedido lp, productos pr
     WHERE p.clientes_id =c.id AND p.id = lp.productos_id AND lp.productos_id = pr.id;
+
+--A partir de aqui lo hice yo Salo
     
 SELECT p.nombre, c.nombre, sub.nombre
     FROM categorias c, productos p, subcategorias sub
